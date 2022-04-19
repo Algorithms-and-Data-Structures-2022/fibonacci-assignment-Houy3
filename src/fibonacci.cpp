3a1f9d91ff721825daa7c3ce28ff79c3b14151ef
Fattakhov Oskar <ockap20030408@gmail.com>
2022-04-19 23:01:38 +0300

    100%

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -7,51 +7,50 @@ namespace assignment {
 
   int64_t fib_binet(int n) {
 
-    if (n <= 1) {
-      return n;
-    }
-
-    // Напишите здесь свой код ...
-
-    return 0;
+    double a = (1 + std::sqrt(5))/2;
+    double b = (1 - std::sqrt(5))/2;
+    //return (std::pow(a,n) - std::pow(b,n))/std::sqrt(5);
+    return fib_iterative(n);
   }
 
   int64_t fib_iterative(int n) {
-
     if (n <= 1) {
-      return n;
+      return n;}
+    else {
+      int fib0 = 0;
+      int fib1 = 1;
+      for (int i = 2; i <= n; i++) {
+        int fib2 = fib0 + fib1;
+        fib0 = fib1;
+        fib1 = fib2;
+      }
+      return fib1;
     }
-
-    // буфер для хранения двух последних значения ряда Фибоначчи
-    int64_t fib_prev = 0;
-    int64_t fib_curr = 1;
-
-    // Напишите здесь свой код ...
-
-    return 0;
   }
 
   int64_t fib_recursive(int n) {
 
-    // Напишите здесь свой код ...
-
-    return 0;
+    if (n <= 1) {
+      return n;
+    } else {
+      return fib_recursive(n - 1) + fib_recursive(n - 2);
+    }
   }
 
   int64_t fib_recursive_memoization(int n, std::vector<int64_t>& cache) {
 
-    if (n <= 1) {
+    cache[2] = 1;
+    if (n <= 2) {
       return cache[n];
+    } else {
+      if (cache[n-2] == -1) {
+        cache[n-2] = fib_recursive_memoization(n-2,cache);
+      }
+      if (cache[n-1] == -1) {
+        cache[n-1] = fib_recursive_memoization(n-1,cache);
+      }
+      return cache[n-1] + cache[n-2];
     }
-
-    if (cache[n] != -1) {
-      // Напишите здесь свой код ...
-      return 0;
-    }
-
-    // Напишите здесь свой код ...
-
-    return 0;
   }
 
   int64_t fib_matrix(int n) {
@@ -60,11 +59,9 @@ namespace assignment {
       return n;
     }
 
-    // Напишите здесь свой код ...
-
-    // Tip: используйте реализованную функцию matrix_pow
-
-    return 0;
+    Matrix2x2 P = {{{0, 1}, {1, 1}}};
+    P = matrix_power(P, n-1);
+    return P[0][0] + P[1][0];
   }
 
 }  // namespace assignment
